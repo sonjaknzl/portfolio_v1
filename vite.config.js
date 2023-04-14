95d72e1f36600b83758ef4fd1520796d60401817
sonjaknzl <sonjakuenzl@aol.com>
2023-04-14 08:23:52 +0200

    config file change

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,1 @@
-import { defineConfig } from "vite";
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      // Make sure to externalize Three.js dependencies
-      external: ["three"],
-
-      output: {
-        // Provide global variables for Three.js dependencies
-        globals: {
-          three: "THREE",
-        },
-      },
-    },
-  },
-});
